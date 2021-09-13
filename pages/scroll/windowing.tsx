9473b6e18a423e93af4b050d80c1c248d40b5c15
Mateusz Jarzębski <matt@teacode.io>
2021-09-13 20:21:12 +0200

    fix ssr

---
+++ pages/scroll/windowing.tsx
@@ -0,0 +1,42 @@
+import Head from 'next/head'
+import styles from '../../styles/LongList.module.css'
+import { useEffect, useState } from 'react'
+import { Data } from '../../interfaces/data.interface'
+
+export default function ScrollBase() {
+  const [data, setData] = useState<Data[]>([])
+  useEffect(() => {
+    ;(async function () {
+      const res = await fetch('/api/list-data')
+      const data = await res.json()
+      setData(data)
+    })()
+  }, [])
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Optimisation React</title>
+        <meta name="description" content="Thesis example" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        {data.map((el) => {
+          return (
+            <div key={el.id}>
+              <h3>
+                {el.firstName} {el.lastName}
+              </h3>
+              <p>{el.lorem}</p>
+              <p>
+                You can reach me here: {el.address.street} {el.address.city} {el.address.code}. <br />
+                <span>{el.phone}</span>
+              </p>
+            </div>
+          )
+        })}
+      </main>
+    </div>
+  )
+}
